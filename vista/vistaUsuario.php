1a87257a273c6725147cfe1b38db16b4500308ed
JoellAndres <joeltuma278@gmail.com>
2022-04-23 18:22:47 -0300

    Avance estructura basica

--- vista/vistaUsuario.php
+++ vista/vistaUsuario.php
@@ -5,7 +5,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Joel.T | Usuario</title>
+        <title>User</title>
         <link rel="stylesheet" type="text/css" href="../css/styles.php">   
         <!-- CSS only -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">    
